6b3ad54cad8db54d28173ce94fbb1bd731f33f86
GH0ST <adamjamessherrill@gmail.com>
2022-09-04 18:22:42 -0500

    added submiit function for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-var cityFormEl = $('#city-form');
-var recentSearchesList = $('#recent-searches');
-var cityInputEl = $('city');
-var 
+var cityFormEl = document.querySelector('#city-form');
+var recentSearchesList = document.querySelector('#recent-searches');
+var cityInputEl = document.querySelector('#city');
+var forcastContainerEl = document.querySelector('#city-container');
+var citySearch = document.querySelector('#city-search');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    
+    var city = cityInputEl.value.trim();
+    console.log(city);
+
+    if (city) {
+        getCityForcast(city);
+
+        forcastContainerEl.textContent = '';
+        cityInputEl.value = '';
+    } else {
+        alert('Please enter a city');
+    }
+}
+
+$(cityFormEl).on('submit', formSubmitHandler);
